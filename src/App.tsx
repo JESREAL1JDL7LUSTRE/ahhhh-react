35e6f796233d06f7e1e171adf2b8a613bea1b4f6
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2025-01-24 12:49:42 +0800

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,25 @@ const App = () => {
 
       <h1 className="flex text-4xl font-bold">Hello</h1>
 
-      <div id="parent" className="border-2 border-blue-800 p-2 size-2/3 flex bg-zinc-900 rounded-3xl shadow-xl mt-10 flex-col">
+      <div id="parent" className="border-2 border-blue-800 p-2 size-2/3 flex bg-zinc-900 rounded-3xl shadow-xl mt-10 flex-col sm:gap-2">
 
         {/* 1st row*/}
-        <div id='groupdiv' className="flex flex-row size-full space-x-2 p-2">
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl justify-center flex font-semibold">wazzap</div>
+        <div id='groupdiv' className="flex md:flex-row flex-col size-full md:space-x-2 md:p-2">
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl flex justify-center">
+            <h1 className=" text-md md:text-3xl xl:text-5xl font-semibold">hello</h1></div>
         </div>
         {/* 2nd row*/}
 
-        <div id='groupdiv' className="flex flex-row size-full space-x-2 p-2">
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center"> </div>
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center"> </div>
+        <div id='groupdiv' className="flex md:flex-row flex-col size-full md:space-x-2 md:p-2 gap-1 ">
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center flex-[1] hover:flex-[3] transition-all duration-1000"> </div>
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center flex-[1] hover:flex-[3] transition-all duration-1000"> </div>
         </div>
         {/* 3rd row*/}
 
-        <div id='groupdiv' className="flex flex-row size-full space-x-2 p-2">
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center"> </div>
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center"> </div>
-          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center"> </div>
+        <div id='groupdiv' className="flex md:flex-row flex-col size-full md:space-x-2 md:p-2 gap-1">
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center flex-[1] hover:flex-[3] transition-all duration-1000"> </div>
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center flex-[1] hover:flex-[3] transition-all duration-1000"> </div>
+          <div id="indiv-div" className="border border-red-600 size-full bg-white rounded-3xl text-center flex-[1] hover:flex-[3] transition-all duration-1000"> </div>
         </div>
         
       </div>
